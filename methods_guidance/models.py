32f0a47c6da126b901f0bd43c153c1d71d109fa1
Matt <matt.snell.00@hotmail.co.uk>
2021-05-10 13:41:48 +0000

    add bare content to textbooks and create textbooks model

--- methods_guidance/models.py
+++ methods_guidance/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Textbook(models.Model):
+    name = models.CharField(max_length=150, blank=False, null=False)
+    image = models.ImageField()
+    image_link = models.URLField()
+    description = models.CharField(max_length=1254, blank=False, null=False)
+    optional_link = models.URLField(blank=True)
+    optional_link_text = models.CharField(max_length=124, blank=True)
+
+    def __str__(self):
+        return self.name
